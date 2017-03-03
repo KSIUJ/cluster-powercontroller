3bb2b4db65ff44ac1b9b18274dd1b21f13b02e08
Adam Pardyl <adam@pardyl.com>
2017-03-03 20:25:24 +0100

    Add AtmelStudio project

---
+++ Firmware/Firmware/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Firmware.cpp
+ *
+ * Created: 2017-03-03 20:23:59
+ * Author : Adam
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
